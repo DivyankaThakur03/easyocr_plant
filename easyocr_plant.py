74c8137befd064514606e576b0b631200d0f70bd
Rajesh Roy <rajeshroy402@gmail.com>
2022-08-29 22:42:45 +0530

    added flags to automate rotation

--- easyocr_plant.py
+++ easyocr_plant.py
@@ -19,23 +19,47 @@ args = parser.parse_args()
 #img = cv2.imread("/home/divya/Downloads/chilli/train/IMG_0344.JPG") #Grade- 2
 
 img = cv2.imread(args.path)
+flag = 0
 #rotating an image so that the label is clear
-img = cv2.rotate(img, cv2.cv2.ROTATE_90_CLOCKWISE)
+if flag==0:
+  img = cv2.rotate(img, cv2.cv2.ROTATE_90_CLOCKWISE)
 #img = cv2.rotate(img, cv2.cv2.ROTATE_180)
 
 #to sharpen the blurness
-sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
-sharpen = cv2.filter2D(img, -1, sharpen_kernel)
-reader = easyocr.Reader(['en'])
-result = reader.readtext(sharpen)
-print("balhhh ",type(result))
+  sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+  sharpen = cv2.filter2D(img, -1, sharpen_kernel)
+  reader = easyocr.Reader(['en'])
+  result = reader.readtext(sharpen)
 
 #access the elements of an array in a list
-for i in result:
-    coordinates = i[0]
-    text = i[1]
-    confi = i[2]
+  for i in result:
+      coordinates = i[0]
+      text = i[1]
+      confi = i[2]
+      
+  if text[:2] == 'GR':
+    flag = 0
+    print("90 it is")
+  else:
+    flag = 1
+ 
+elif flag == 1:
+  img = cv2.rotate(img, cv2.cv2.ROTATE_90_CLOCKWISE)
+  sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+  sharpen = cv2.filter2D(img, -1, sharpen_kernel)
+  reader = easyocr.Reader(['en'])
+  result = reader.readtext(sharpen)
 
-print(result)
+#access the elements of an array in a list
+  for i in result:
+      coordinates = i[0]
+      text = i[1]
+      confi = i[2]
+  
+  if text[:2] == 'GR':
+    print("180 it is")
+  else:
+    print("Sorry - this image is too tough for me")
+    
 
-print("coordinates : {}, text : {}, confi : {}".format(type(coordinates), type(text), type(confi)))
+print("coordinates : {}, text : {}, confi : {}".format(coordinates, text, confi))
