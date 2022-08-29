0a27a4957947e6baf4d92f1667a8d2c567edaa4d
DivyankaThakur03 <85764700+DivyankaThakur03@users.noreply.github.com>
2022-08-29 22:06:57 +0530

    Update easyocr_plant.py

--- easyocr_plant.py
+++ easyocr_plant.py
@@ -7,14 +7,19 @@ import numpy as np
 #img = cv2.imread("/home/divya/Downloads/chilli/train/IMG_0431.JPG") #Grade- 3
 img = cv2.imread("/home/divya/Downloads/chilli/train/IMG_0344.JPG") #Grade- 2
 
+#rotating an image so that the label is clear
 img = cv2.rotate(img, cv2.cv2.ROTATE_90_CLOCKWISE)
 #img = cv2.rotate(img, cv2.cv2.ROTATE_180)
+
+#to sharpen the blurness
 sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
 sharpen = cv2.filter2D(img, -1, sharpen_kernel)
 reader = easyocr.Reader(['en'])
 result = reader.readtext(sharpen)
 #print("balhhh ",type(result))
+
+#access the elements of an array in a list
 for i in result:
     print("blahh",i[0])
-#print("demo",len(result))
+#print("length ",len(result))
 print(result)
