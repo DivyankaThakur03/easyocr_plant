5ee1b7184d89ec42163c9428a50f381c34cdbfff
DivyankaThakur03 <85764700+DivyankaThakur03@users.noreply.github.com>
2022-08-30 12:27:10 +0530

    Update easyocr_plant.py

--- easyocr_plant.py
+++ easyocr_plant.py
@@ -44,7 +44,7 @@ if flag==0:
     print("90 it is")
   elif text[:2] == ' ':
     flag = 1
-print("hi") 
+    print("hi") 
 elif flag==1:
   img180 = cv2.rotate(img, cv2.ROTATE_180)
   #cv2.imshow("180",img180)
