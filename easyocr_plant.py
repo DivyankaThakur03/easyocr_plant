34f963aebd41668a82432cb343ca7d9aa1484552
DivyankaThakur03 <85764700+DivyankaThakur03@users.noreply.github.com>
2022-08-30 12:08:09 +0530

    Update easyocr_plant.py

--- easyocr_plant.py
+++ easyocr_plant.py
@@ -24,7 +24,7 @@ img = cv2.imread(args.path)
 flag = 0
 #rotating an image so that the label is clear
 if flag==0:
-  img90 = cv2.rotate(img, cv2.cv2.ROTATE_90_CLOCKWISE)
+  img90 = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
   #cv2.imshow("90", img90)
 
 #to sharpen the blurness
@@ -46,7 +46,7 @@ if flag==0:
     flag = 1
  
 elif flag==1:
-  img180 = cv2.rotate(img, cv2.cv2.ROTATE_180)
+  img180 = cv2.rotate(img, cv2.ROTATE_180)
   #cv2.imshow("180",img180)
   sharpen_kernel180 = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
   sharpen180 = cv2.filter2D(img180, -1, sharpen_kernel180)
